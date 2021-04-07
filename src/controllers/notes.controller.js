36c862130b384104c6eda19bec197c03e8dc6a78
EduardoMay <eduardo_may@outlook.com>
2021-04-07 16:06:18 -0500

    feat: obtener nota por id

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,5 +1,10 @@
 import Notes from "../models/Notes";
 
+/**
+ * Get all notes
+ * @param   Request  req
+ * @param   Response  res
+ */
 export const getNotes = async (req, res) => {
   try {
     const notes = await Notes.find();
@@ -7,5 +12,27 @@ export const getNotes = async (req, res) => {
     res.status(200).json(notes);
   } catch (error) {
     res.status(400).json({ message: "UPS!" });
+  }
+};
+
+/**
+ * Get note by Id
+ *
+ * @param   Request  req
+ * @param   Response  res
+ */
+export const getNotesById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const note = await Notes.findById(id);
+
+    if (!note) return res.status(400).send({ message: "Nota no encontrada" });
+
+    res.status(200).json(note);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || "OPS!"
+    });
   }
 };
